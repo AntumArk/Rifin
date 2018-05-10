3f52daeeff1ad66d1b8a4eb7ccc61a01c882e3a0
Zinc <minkevicius.linas@gmail.com>
2018-05-10 07:51:26 +0300

    Image saving and loading

--- Rifin/Form1.cs
+++ Rifin/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -29,6 +30,19 @@ namespace Rifin
             InitializeComponent();
             Source = new Mat(new OpenCvSharp.Size(640, 480), MatType.CV_8U,Scalar.White);
             addDescriptorControl1.Hide();
+
+            Environment.CurrentDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+          var directories= Directory.EnumerateDirectories(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisualData")).ToList();
+            foreach (var dir in directories)
+            {
+               TrainingObjects.Add( dir.Substring(dir.LastIndexOf("\\") + 1));
+            }
+            richTextBox1.ResetText();
+            if (TrainingObjects != null)
+                foreach (var obj in TrainingObjects)
+                    if (obj != null)
+                        richTextBox1.AppendText(obj.ToString() + "\n");
 
             videoStreamThread = new Thread(videoStreamT);
             mainThread = new Thread(mainT);
@@ -91,7 +105,11 @@ namespace Rifin
                 capture.Read(src);
                 
                 src.CopyTo(Source);
-                src = DetectFeatures(src);
+                // src = DetectFeatures(src);
+                foreach (var obj in TrainingObjects)
+                {
+                    GetGoodMatches(src, obj);
+                }
                     Main_PictureBox.Image = src.ToBitmap();
                 
                 
@@ -109,66 +127,80 @@ namespace Rifin
            
         }
       
-        private DMatch GetGoodMatches(Mat source,string objectName)
+        private void GetGoodMatches(Mat source,string objectName)
         {
+            //Step one: 
+            //Open folder of an object
+            var workPath= Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"VisualData", objectName);
+            Environment.CurrentDirectory = workPath;
+            //Step two:
+            //Load a list of items
+            var imgFiles = Directory.GetFileSystemEntries("Images");
+            var keypointFiles = Directory.GetFileSystemEntries("Keypoints");
 
-            Mat img_1 = Cv2.ImRead("../../Images/icons.png", ImreadModes.GrayScale);
-            Mat img_2 = Cv2.ImRead("../../Images/subIcons.png", ImreadModes.GrayScale);
-
-            // Step 1: Detect the keypoints using SURF Detector, compute the descriptors
-            int minHessian = 400;
-           SURF detector = SURF.Create(minHessian);
-
-            KeyPoint[] keypoints_1, keypoints_2;
-            Mat descriptors_1 = new Mat(), descriptors_2 = new Mat();
-
-            detector.DetectAndCompute(img_1, new Mat(), out keypoints_1, descriptors_1);
-            detector.DetectAndCompute(img_2, new Mat(), out keypoints_2, descriptors_2);
-
-            ////-- Step 2: Matching descriptor vectors using FLANN matcher
-            FlannBasedMatcher matcher = new FlannBasedMatcher();
-            DMatch[] matches;
-
-            matches = matcher.Match(descriptors_1, descriptors_2);
-            double max_dist = 0; double min_dist = 100;
-
-            ////-- Quick calculation of max and min distances between keypoints
-           for (int i = 0; i < descriptors_1.Rows; i++)
+            // var fileName = "Keypoints_" + fileNames[i] + ".yml"
+            foreach (var imgFile in imgFiles)
             {
-                double dist = matches[i].Distance;
-                if (dist < min_dist) min_dist = dist;
-               if (dist > max_dist) max_dist = dist;
+                Mat img_1 = Cv2.ImRead("../../Images/icons.png", ImreadModes.GrayScale);
+                Mat img_2 = Cv2.ImRead("../../Images/subIcons.png", ImreadModes.GrayScale);
+
+                // Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+                int minHessian = 400;
+                SURF detector = SURF.Create(minHessian);
+
+                KeyPoint[] keypoints_1, keypoints_2;
+                Mat descriptors_1 = new Mat(), descriptors_2 = new Mat();
+
+                detector.DetectAndCompute(img_1, new Mat(), out keypoints_1, descriptors_1);
+                detector.DetectAndCompute(img_2, new Mat(), out keypoints_2, descriptors_2);
+
+                ////-- Step 2: Matching descriptor vectors using FLANN matcher
+                FlannBasedMatcher matcher = new FlannBasedMatcher();
+                DMatch[] matches;
+
+                matches = matcher.Match(descriptors_1, descriptors_2);
+                double max_dist = 0; double min_dist = 100;
+
+                ////-- Quick calculation of max and min distances between keypoints
+                for (int i = 0; i < descriptors_1.Rows; i++)
+                {
+                    double dist = matches[i].Distance;
+                    if (dist < min_dist) min_dist = dist;
+                    if (dist > max_dist) max_dist = dist;
+                }
+                Console.WriteLine("-- Max dist : %f", max_dist);
+                Console.WriteLine("-- Min dist : %f", min_dist);
+
+                ////-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+                ////-- or a small arbitrary value ( 0.02 ) in the event that min_dist is very
+                ////-- small)  //-- PS.- radiusMatch can also be used here.
+                List<DMatch> good_matches = new List<DMatch>();
+                for (int i = 0; i < descriptors_1.Rows; i++)
+                {
+                    if (matches[i].Distance <= Math.Max(2 * min_dist, 0.02))
+                    {
+                        good_matches.Add(matches[i]);
+                    }
+                }
+
+                ////-- Draw only "good" matches
+                Mat img_matches = new Mat();
+                Cv2.DrawMatches(img_1, keypoints_1, img_2, keypoints_2,
+                    good_matches, img_matches, Scalar.All(-1), Scalar.All(-1),
+                    new List<byte>(), DrawMatchesFlags.NotDrawSinglePoints);
+
+                ////-- Show detected matches  imshow( "Good Matches", img_matches );
+                for (int i = 0; i < (int)good_matches.Count; i++)
+                {
+                    Console.WriteLine("-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d", i,
+                        good_matches[i].QueryIdx, good_matches[i].TrainIdx);
+                }
+
+                Cv2.WaitKey(0);
             }
-            Console.WriteLine("-- Max dist : %f", max_dist);
-            Console.WriteLine("-- Min dist : %f", min_dist);
 
-            ////-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-            ////-- or a small arbitrary value ( 0.02 ) in the event that min_dist is very
-            ////-- small)  //-- PS.- radiusMatch can also be used here.
-            List<DMatch> good_matches = new List<DMatch>();
-            for (int i = 0; i < descriptors_1.Rows; i++)
-            {
-               if (matches[i].Distance <= Math.Max(2 * min_dist, 0.02))
-               {
-                   good_matches.Add(matches[i]);
-               }
-            }
 
-            ////-- Draw only "good" matches
-            Mat img_matches = new Mat();
-            Cv2.DrawMatches(img_1, keypoints_1, img_2, keypoints_2,
-                good_matches, img_matches, Scalar.All(-1), Scalar.All(-1),
-                new List<byte>(), DrawMatchesFlags.NotDrawSinglePoints);
-
-            ////-- Show detected matches  imshow( "Good Matches", img_matches );
-            for (int i = 0; i < (int)good_matches.Count; i++)
-            {
-                Console.WriteLine("-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d", i,
-                    good_matches[i].QueryIdx, good_matches[i].TrainIdx);
-            }
-
-            Cv2.WaitKey(0);
-
+           
 
         }
 
--- Rifin/Forms/AddDescriptorControl.cs
+++ Rifin/Forms/AddDescriptorControl.cs
@@ -34,39 +34,48 @@ namespace Rifin.Forms
 
         private void LoadResourcesButton_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog
-            {
-                Multiselect = true,
-                InitialDirectory = @"E:\Coding Workspace\TrainingData\Rifin",
-                Filter = "image files (*.*)|*.*",
-                FilterIndex = 2,
-                RestoreDirectory = true
-            };
+            if(ObjectName=="")
+            { MessageBox.Show("Please enter the name first"); } else
 
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                int i = 0;
-                keypointsCount = 0;
-                var files = openFileDialog1.FileNames;
-                ResourcesProgressBar.Step = 1;
-                ResourcesProgressBar.Maximum = files.Length;
 
-                ResourcesProgressBar.Value = 0;
-                foreach (var file in files)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog
                 {
-                    Mat img = Cv2.ImRead(file);
-                    //  img.ConvertTo(img, MatType.CV_32FC3);
-                    Cv2.Resize(img, img, new OpenCvSharp.Size(640, 480));
-                    ObjectImages.Add(img);
-                    ObjectKeyPoints.Add(DetectFeatures(img));
-                    keypointsCount += ObjectKeyPoints[i].Length;
-                    fileNames.Add(i.ToString());
-                    i++;
-                    ResourcesProgressBar.Value += ResourcesProgressBar.Step;
+                    Multiselect = true,
+                    InitialDirectory = @"E:\Coding Workspace\TrainingData\Rifin",
+                    Filter = "image files (*.*)|*.*",
+                    FilterIndex = 2,
+                    RestoreDirectory = true
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VisualData", ObjectName, "Images"));
+                    int i = 0;
+                    keypointsCount = 0;
+                    var files = openFileDialog1.FileNames;
+                    ResourcesProgressBar.Step = 1;
+                    ResourcesProgressBar.Maximum = files.Length;
+
+                    ResourcesProgressBar.Value = 0;
+                    foreach (var file in files)
+                    {
+                        Mat img = Cv2.ImRead(file);
+                        //  img.ConvertTo(img, MatType.CV_32FC3);
+                        Cv2.Resize(img, img, new OpenCvSharp.Size(640, 480));
+                        ObjectImages.Add(img);
+                        ObjectKeyPoints.Add(DetectFeatures(img));
+                        keypointsCount += ObjectKeyPoints[i].Length;
+                        fileNames.Add(i.ToString());
+                      
+                        img.SaveImage(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"VisualData",ObjectName, "Images", i + ".jpg"));
+                        i++;
+                        ResourcesProgressBar.Value += ResourcesProgressBar.Step;
+                    }
+                    LoadedImagesLabel.Text = i.ToString();
+                    FoundKeypointsLabel.Text = keypointsCount.ToString();
+                    ;
                 }
-                LoadedImagesLabel.Text =  i.ToString();
-                FoundKeypointsLabel.Text = keypointsCount.ToString() ;
-                ;
             }
         }
         /// <summary>
